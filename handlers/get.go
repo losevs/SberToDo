76bbe0189333680bb7daaa8be9ad60cacc08e23f
Sergey <hi.sergeyl@gmail.com>
2023-09-20 13:52:46 +0300

    add model request & fix add handler

--- handlers/get.go
+++ handlers/get.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"sber/database"
 	"sber/models"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Show(c *fiber.Ctx) error {
 	query := []models.ToDo{}
-	if check := database.DB.Db.Find(&query); check.RowsAffected == 0 {
+	if check := database.DB.Db.Order("date asc").Find(&query); check.RowsAffected == 0 {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "todo list is empty",
 		})
@@ -19,15 +22,62 @@ func Show(c *fiber.Ctx) error {
 }
 
 func Add(c *fiber.Ctx) error {
-	query := models.ToDo{}
+	query := models.ToDoRequest{}
 	if err := c.BodyParser(&query); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": err,
 		})
 	}
-	database.DB.Db.Create(&query)
+	// Check Title if exists
+	emptyQuery := new(models.ToDo)
+	if check := database.DB.Db.Where("title = ?", query.Title).First(&emptyQuery); check.RowsAffected != 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"todo":    emptyQuery,
+			"message": "this title already exisits",
+		})
+	}
+
+	// Check Date
+	splitDate := strings.Split(query.Date, ".")
+	if len(splitDate) != 3 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "bad date request: need dd.mm.yyyy",
+		})
+	}
+	//check year
+	if len(splitDate[2]) != 4 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "bad year request",
+		})
+	}
+	day, checkNum1 := strconv.Atoi(splitDate[0])
+	if day < 1 || day > 31 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "bad date request: wrong day number",
+		})
+	}
+	month, checkNum2 := strconv.Atoi(splitDate[1])
+	if month < 1 || month > 12 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "bad date request: wrong month number",
+		})
+	}
+	year, checkNum3 := strconv.Atoi(splitDate[2])
+	if checkNum1 != nil || checkNum2 != nil || checkNum3 != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "bad date request: need dd.mm.yyyy in numbers",
+		})
+	}
+	DateAdd := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+	queryToAdd := models.ToDo{
+		Title:       query.Title,
+		Description: query.Description,
+		Date:        DateAdd,
+		Flag:        query.Flag,
+	}
+	database.DB.Db.Create(&queryToAdd)
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"todo":    query,
+		"todo":    queryToAdd,
 		"message": "todo created",
 	})
 }
--- models/models.go
+++ models/models.go
@@ -1,6 +1,15 @@
 package models
 
+import "time"
+
 type ToDo struct {
+	Title       string    `json:"title"`
+	Description string    `json:"desc"`
+	Date        time.Time `json:"date" gorm:"type:timestamp"`
+	Flag        bool      `json:"flag"`
+}
+
+type ToDoRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"desc"`
 	Date        string `json:"date"`
